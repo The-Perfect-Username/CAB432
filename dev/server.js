366645d91495d16abf1eef6956b9365d8b6a6097
Lachlan Enderlin <lendo1995@gmail.com>
2016-08-30 21:53:50 +1000

    Ok, created a new project and things seem alright
    Able to route from views, use CSS and JS files, and route

---
+++ dev/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path')
+var http = require('http');
+var app = express();
+
+
+const hostname = "localhost";
+const port = 3000;
+
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/views/index.html');
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.listen(port, function () {
+	console.log("Express app listening at http://" + hostname + ":" + port + "/");
+});
