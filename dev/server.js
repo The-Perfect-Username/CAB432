ce09a15240d19d569a92bc1fc6c331f901763a03
Lachlan Enderlin <lendo1995@gmail.com>
2016-09-03 15:09:13 +1000

    Removed instagram because bullshit

--- dev/server.js
+++ dev/server.js
@@ -2,12 +2,10 @@ var express = require('express');
 var path = require('path')
 var http = require('http');
 var app = express();
-//var instagram = require("instagram-node").instagram();
 
 
 const hostname = "localhost";
 const port = 3000;
-
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/index.html');
