c1f0165cfa540f7e4ba3d32f9715c9c743674630
Lachlan Enderlin <lendo1995@gmail.com>
2016-09-09 00:56:03 +1000

    Now able to show icons by clicking the results

--- dev/public/js/default.js
+++ dev/public/js/default.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
 	$("form").on("submit", function(e) {
 		e.preventDefault();
 		var value = $("input#search-bar").val();
@@ -19,16 +18,62 @@ $(document).ready(function() {
 	});
 
 	function thing(data) {
-    	var html = "";
-    	var result;
-    	var rating_img;
-    	for (var i = 0; i < data.length; i++) {
-    		rating_img = "<img src='" + data[i].rating_img_url + "' alt='ratings' />";
-    		result = data[i].name + ", Reviews: " + data[i].review_count + ", Address: " + data[i].location.display_address[0] + ", " + data[i].location.display_address[1];
-    		html += "<div class='result'><p>" + result + "</p>" + rating_img + "</div>";
-    	}
-
-    	return html;
+        var html = "";
+        var result;
+        var name, rating_img, num_of_reviews, preview_image, address, url, coords;
+        for (var i = 0; i < data.length; i++) {
+            rating_img = data[i].rating_img_url;
+            name = data[i].name; 
+            num_of_reviews = data[i].review_count;
+            address =  data[i].location.display_address[0] + ", " + data[i].location.display_address[1];
+            preview_image = data[i].image_url;
+            url = data[i].url;
+            coords = data[i].location.coordinate.latitude + "," + data[i].location.coordinate.longitude;
+            html += result_design(name, rating_img, num_of_reviews, address, preview_image, url, coords);
+        }
+        return html;
     }
 
+    function result_design(name, rating, reviews, address, preview, url, coords) {
+        html = "<div class='result' rel='" + coords + "'>";
+        html += "<div class='result-info'>";
+        html += "<h4 class='result-name'>" + name + "</h4>";
+        html += "<ul class='inline-list'>";
+        html += "<li class='inline-item'>";
+        html += "<span><img src='" + rating + "' alt='rating'></span>";
+        html += "</li>"
+        html += "<li class='inline-item light-grey'>";
+        html += "(" + reviews + ")";
+        html += "</li>";
+        html += "<li class='inline-item light-grey'>";
+        html += "<a class='yelp-link light-grey' href='" + url + "' target='_blank' title='View on Yelp'><i class='fa fa-yelp' aria-hidden='true'></i></a>";
+        html += "</li>";
+        html += "</ul>";
+        html += "<p class='address-info grey'>" + address + "</p>";
+        html += "</div>";
+        html += "<div class='image-wrapper'>";
+        html += "<img class='preview-img' src='" + preview + "' />";
+        html += "</div>";
+        html += "</div>";
+
+        return html;
+    }
 });
+
+function show_result_icon_on_click(map) {
+    $(document).on("click", ".result", function() {
+        var coords = $(this).attr("rel");
+        coords     = coords.split(",");
+        var latitude = parseFloat(coords[0]);
+        var longitude = parseFloat(coords[1]);
+        var pos = {
+            lat: latitude,
+            lng: longitude
+        };
+        // Creates a new marker
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map
+        });
+    });
+}
--- dev/public/js/map.js
+++ dev/public/js/map.js
@@ -74,75 +74,74 @@ function initMap() {
 
 
 	    ajax_instance(latitude, longitude);
-
-	    
-	    
 	    
 	});
 
 	function ajax_instance(latitude, longitude) {
-		    	// jQuery
-		    $(function(){
-		    	var coords = latitude + ',' + longitude;
-		    	coords = coords.toString();
-		    	$.ajax({
-		    		type: 'GET',
-		    		url: "http://localhost:3000/yelp/bycoord/bars/" + coords,
-		    		success: function(data) {
-		    			var result = data[0].rating + ", " + data[0].name;
-		    			var len = data.length;
-		    			console.log(len);
-		    			var t = thing(data);
-		    			$('div#text').html(t);
-		    		}
+	    	// jQuery
+	    $(function(){
+	    	var coords = latitude + ',' + longitude;
+	    	coords = coords.toString();
+	    	$.ajax({
+	    		type: 'GET',
+	    		url: "http://localhost:3000/yelp/bycoord/bars/" + coords,
+	    		success: function(data) {
+	    			var result = data[0].rating + ", " + data[0].name;
+	    			var len = data.length;
+	    			console.log(len);
+	    			var t = thing(data);
+	    			$('div#text').html(t);
+	    		}
 
-		    	});
-		    	console.log( latitude + ', ' + longitude );
-		    	
-		    });
-	    }
+	    	});
+	    	console.log( latitude + ', ' + longitude );
+	    	
+	    });
+    }
 
-	    function thing(data) {
-	    	var html = "";
-	    	var result;
-	    	var name, rating_img, num_of_reviews, preview_image, address, url;
-	    	for (var i = 0; i < data.length; i++) {
-	    		rating_img = data[i].rating_img_url;
-	    		name = data[i].name; 
-	    		num_of_reviews = data[i].review_count;
-	    		address =  data[i].location.display_address[0] + ", " + data[i].location.display_address[1];
-	    		preview_image = data[i].image_url;
-	    		url = data[i].url;
-	    		html += result_design(name, rating_img, num_of_reviews, address, preview_image, url);
-	    	}
-	    	return html;
-	    }
+    function thing(data) {
+    	var html = "";
+    	var result;
+    	var name, rating_img, num_of_reviews, preview_image, address, url, coords;
+    	for (var i = 0; i < data.length; i++) {
+    		rating_img = data[i].rating_img_url;
+    		name = data[i].name; 
+    		num_of_reviews = data[i].review_count;
+    		address =  data[i].location.display_address[0] + ", " + data[i].location.display_address[1];
+    		preview_image = data[i].image_url;
+    		url = data[i].url;
+    		coords = data[i].location.coordinate.latitude + "," + data[i].location.coordinate.longitude;
+    		html += result_design(name, rating_img, num_of_reviews, address, preview_image, url, coords);
+    	}
+    	return html;
+    }
 
-	    function result_design(name, rating, reviews, address, preview, url) {
-	    	html = "<div class='result'>";
-	    	html += "<div class='result-info'>";
-			html += "<h4 class='result-name'>" + name + "</h4>";
-			html += "<ul class='inline-list'>";
-			html += "<li class='inline-item'>";
-			html += "<span><img src='" + rating + "' alt='rating'></span>";
-			html += "</li>"
-			html += "<li class='inline-item light-grey'>";
-			html += "(" + reviews + ")";
-			html += "</li>";
-			html += "<li class='inline-item light-grey'>";
-			html += "<a class='yelp-link light-grey' href='" + url + "' target='_blank' title='View on Yelp'><i class='fa fa-yelp' aria-hidden='true'></i></a>";
-			html += "</li>";
-			html += "</ul>";
-			html += "<p class='address-info grey'>" + address + "</p>";
-			html += "</div>";
-			html += "<div class='image-wrapper'>";
-			html += "<img class='preview-img' src='" + preview + "' />";
-			html += "</div>";
-			html += "</div>";
+    function result_design(name, rating, reviews, address, preview, url, coords) {
+        html = "<div class='result' rel='" + coords + "'>";
+        html += "<div class='result-info'>";
+        html += "<h4 class='result-name'>" + name + "</h4>";
+        html += "<ul class='inline-list'>";
+        html += "<li class='inline-item'>";
+        html += "<span><img src='" + rating + "' alt='rating'></span>";
+        html += "</li>"
+        html += "<li class='inline-item light-grey'>";
+        html += "(" + reviews + ")";
+        html += "</li>";
+        html += "<li class='inline-item light-grey'>";
+        html += "<a class='yelp-link light-grey' href='" + url + "' target='_blank' title='View on Yelp'><i class='fa fa-yelp' aria-hidden='true'></i></a>";
+        html += "</li>";
+        html += "</ul>";
+        html += "<p class='address-info grey'>" + address + "</p>";
+        html += "</div>";
+        html += "<div class='image-wrapper'>";
+        html += "<img class='preview-img' src='" + preview + "' />";
+        html += "</div>";
+        html += "</div>";
 
-			return html;
-	    }
+        return html;
+    }
 
+    show_result_icon_on_click(map);
 
 }
 
